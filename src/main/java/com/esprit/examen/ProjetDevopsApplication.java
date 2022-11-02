df8b907a62ac0c1d6b8c3fcedf9a9b024e3faf40
Eskander JGUIRIM <47850204+eskander1998@users.noreply.github.com>
2022-11-02 18:16:31 +0100

    first commit

---
+++ src/main/java/com/esprit/examen/ProjetDevopsApplication.java
@@ -0,0 +1,15 @@
+package com.esprit.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class ProjetDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDevopsApplication.class, args);
+	}
+
+}
