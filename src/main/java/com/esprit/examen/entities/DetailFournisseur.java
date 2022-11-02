0e2c15e10f86a98d37d0b7af460a4c27780f30da
Fourat888 <fourat.anane@esprit.tn>
2022-11-02 21:23:20 +0100

    facture-detail-reglement

---
+++ src/main/java/com/esprit/examen/entities/DetailFournisseur.java
@@ -0,0 +1,76 @@
+package com.esprit.examen.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DetailFournisseur implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idDetailFournisseur;
+	private String email;
+	@Temporal(TemporalType.DATE)
+	private Date dateDebutCollaboration;
+	private String adresse;
+	private String matricule;
+	@OneToOne(mappedBy="detailFournisseur")
+	@JsonIgnore
+	private Fournisseur fournisseur;
+	public Long getIdDetailFournisseur() {
+		return idDetailFournisseur;
+	}
+	public void setIdDetailFournisseur(Long idDetailFournisseur) {
+		this.idDetailFournisseur = idDetailFournisseur;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDateDebutCollaboration() {
+		return dateDebutCollaboration;
+	}
+	public void setDateDebutCollaboration(Date dateDebutCollaboration) {
+		this.dateDebutCollaboration = dateDebutCollaboration;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public Fournisseur getFournisseur() {
+		return fournisseur;
+	}
+	public void setFournisseur(Fournisseur fournisseur) {
+		this.fournisseur = fournisseur;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
---
+++ src/main/java/com/esprit/examen/entities/Facture.java
@@ -0,0 +1,105 @@
+package com.esprit.examen.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Facture implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private float montantRemise;
+	private float montantFacture;
+	@Temporal(TemporalType.DATE)
+	private Date dateCreationFacture;
+	@Temporal(TemporalType.DATE)
+	private Date dateDerniereModificationFacture;
+	private Boolean archivee;
+	@OneToMany(mappedBy = "facture")
+	private Set<DetailFacture> detailsFacture;
+    @ManyToOne
+    @JsonIgnore
+    private Fournisseur fournisseur;
+    @OneToMany(mappedBy="facture")
+    @JsonIgnore
+    private Set<Reglement> reglements;
+    private Long idFacture;
+	public Long getIdFacture() {
+		return idFacture;
+	}
+	public void setIdFacture(Long idFacture) {
+		this.idFacture = idFacture;
+	}
+	public float getMontantRemise() {
+		return montantRemise;
+	}
+	public void setMontantRemise(float montantRemise) {
+		this.montantRemise = montantRemise;
+	}
+	public float getMontantFacture() {
+		return montantFacture;
+	}
+	public void setMontantFacture(float montantFacture) {
+		this.montantFacture = montantFacture;
+	}
+	public Date getDateCreationFacture() {
+		return dateCreationFacture;
+	}
+	public void setDateCreationFacture(Date dateCreationFacture) {
+		this.dateCreationFacture = dateCreationFacture;
+	}
+	public Date getDateDerniereModificationFacture() {
+		return dateDerniereModificationFacture;
+	}
+	public void setDateDerniereModificationFacture(Date dateDerniereModificationFacture) {
+		this.dateDerniereModificationFacture = dateDerniereModificationFacture;
+	}
+	public Boolean getArchivee() {
+		return archivee;
+	}
+	public void setArchivee(Boolean archivee) {
+		this.archivee = archivee;
+	}
+	public Set<DetailFacture> getDetailsFacture() {
+		return detailsFacture;
+	}
+	public void setDetailsFacture(Set<DetailFacture> detailsFacture) {
+		this.detailsFacture = detailsFacture;
+	}
+	public Fournisseur getFournisseur() {
+		return fournisseur;
+	}
+	public void setFournisseur(Fournisseur fournisseur) {
+		this.fournisseur = fournisseur;
+	}
+	public Set<Reglement> getReglements() {
+		return reglements;
+	}
+	public void setReglements(Set<Reglement> reglements) {
+		this.reglements = reglements;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
