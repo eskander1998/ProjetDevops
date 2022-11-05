3cf53ba092814fa550685e0c1a9d47ad6b3f32d4
karoui ahmed <ahmed.karoui@esprit.tn>
2022-11-05 02:49:19 +0100

    update Jenkinsfile1.5

--- src/test/java/com/esprit/examen/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/StockServiceImplTest.java
@@ -3,26 +3,60 @@ package com.esprit.examen;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.StockRepository;
 import com.esprit.examen.services.IStockService;
+import com.esprit.examen.services.StockServiceImpl;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
-@SpringBootTest
+@SpringBootTest(classes=StockServiceImpl.class)
 @ExtendWith(MockitoExtension.class)
 public class StockServiceImplTest {
-	
+	@Mock
+    StockRepository stockRepository;
 	@InjectMocks
+    StockServiceImpl StI;
 	@Autowired
 	IStockService stockService;
+	
+	 Stock stock = new Stock("libelleStock1", 10, 3);
+	    List<Stock> listStocks = new ArrayList<Stock>() {
+	        {
+	            add(new Stock("libelleStock2", 20, 5));
+	            add(new Stock("libelleStock3", 30, 8));
+	        }
+	    };
+	
+	
+	@Test
+	void retrieveStock() {
+	    Mockito.when(stockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(stock));
+	    Stock stock1 = StI.retrieveStock(0L);
+	    Assertions.assertNotNull(stock1);
+	}
+	@Test
+    void retrieveAllStocks(){
+        Mockito.when(stockRepository.findAll()).thenReturn(listStocks);
+        List<Stock> list = StI.retrieveAllStocks();
+        Assertions.assertNotNull(list);
+    }
 	
 	@Test
 	public void testAddStock() {
