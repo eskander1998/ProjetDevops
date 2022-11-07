f37bdaedb4b1dabe5ecd7931fbf9196bfc5e585c
karoui ahmed <ahmed.karoui@esprit.tn>
2022-11-07 06:46:40 +0100

    update projet 30

--- src/test/java/com/esprit/examen/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/ProduitServiceImplTest.java
@@ -39,64 +39,57 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
-
-
 public class ProduitServiceImplTest {
-	
-	@Autowired
-    IProduitService produitService;
+    @Autowired
+       IProduitService produitService;
 
+       @Test
+       public void testRetrieveAllProduit() throws ParseException {
 
-    @Test
-    public void testRetrieveAllProduit() throws ParseException {
+         List<Produit> produit = produitService.retrieveAllProduits();
+         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+         Date date11 = dateFormat.parse("30/09/2000");
+         Date date12 = dateFormat.parse("30/09/2010");
+         Produit sa = new Produit(date11, date12);
+         Produit savedProduit = produitService.addProduit(sa);
+         log.info(" Produit : " + sa);
 
-      List<Produit> produit = produitService.retrieveAllProduits();
-      SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-      Date date11 = dateFormat.parse("30/09/2000");
-      Date date12 = dateFormat.parse("30/09/2010");
-      Produit sa = new Produit(date11, date12);
-      Produit savedProduit = produitService.addProduit(sa);
-      //log.info(" Produit : " + sa);
+         assertNotNull(sa.getIdProduit());
 
-      assertNotNull(sa.getIdProduit());
+         produitService.retrieveProduit(sa.getIdProduit());
 
-      produitService.retrieveProduit(sa.getIdProduit());
-
-      List<Produit> Produits = produitService.retrieveAllProduits();
-      //log.info("Les  Produits : " + Produits);
-   }
-@Test
-public void testDeleteProduit() throws ParseException {
+         List<Produit> Produits = produitService.retrieveAllProduits();
+         log.info("Les  Produits : " + Produits);
+      }
+   @Test
+   public void testDeleteProduit() throws ParseException {
 /*
-   List<Produit> Produits = produitService.retrieveAllProduits();
-      int expected = Produits.size();
-      Long idFour=(long) 2;
-      //assertEquals(expected + 1, produitService.retrieveAllProduits().size());
-   System.out.print("size "+Produits.size());
-      log.info(" count" + Produits.size());
-   produitService.deleteProduit(1L);
-   log.info("size2 "+produitService.retrieveAllProduits().size());
+      List<Produit> Produits = produitService.retrieveAllProduits();
+         int expected = Produits.size();
+         Long idFour=(long) 2;
+         //assertEquals(expected + 1, produitService.retrieveAllProduits().size());
+      System.out.print("size "+Produits.size());
+         log.info(" count" + Produits.size());
+      produitService.deleteProduit(1L);
+      log.info("size2 "+produitService.retrieveAllProduits().size());
 */
 
-}
-@Test
-public void testAddProduit() throws ParseException{
-   List<Produit> Produits = produitService.retrieveAllProduits();
-   SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-   Date date1 = dateFormat.parse("10/02/2020");
-   Date date2 = dateFormat.parse("12/09/2022");
-   Produit p = new Produit(date1, date2);
-   //log.info("produit \n "+p);
+   }
+   @Test
+   public void testAddProduit() throws ParseException{
+      List<Produit> Produits = produitService.retrieveAllProduits();
+      SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+      Date date1 = dateFormat.parse("10/02/2020");
+      Date date2 = dateFormat.parse("12/09/2022");
+      Produit p = new Produit(date1, date2);
+      log.info("produit \n "+p);
 
-   Produit savedProduit= produitService.addProduit(p);
-   //log.info("size1 "+Produits.size());
-   System.out.print("size1 "+Produits.size());
-   produitService.deleteProduit(savedProduit.getIdProduit());
-   List<Produit> Produits1 = produitService.retrieveAllProduits();
+      Produit savedProduit= produitService.addProduit(p);
+      log.info("size1 "+Produits.size());
+      System.out.print("size1 "+Produits.size());
+      produitService.deleteProduit(savedProduit.getIdProduit());
+      List<Produit> Produits1 = produitService.retrieveAllProduits();
 
-   log.info("size2 "+Produits1.size());
-}
-
-	
-
+      log.info("size2 "+Produits1.size());
+   }
 }
