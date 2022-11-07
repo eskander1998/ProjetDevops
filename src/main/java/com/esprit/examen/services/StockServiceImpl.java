e18b930a23779c361ff5032ca0edf39c6fe09665
karoui ahmed <ahmed.karoui@esprit.tn>
2022-11-07 05:05:11 +0100

    update projet 23

--- src/main/java/com/esprit/examen/services/StockServiceImpl.java
+++ src/main/java/com/esprit/examen/services/StockServiceImpl.java
@@ -20,12 +20,12 @@ public class StockServiceImpl implements IStockService {
 	@Override
 	public List<Stock> retrieveAllStocks() {
 		// récuperer la date à l'instant t1
-		//log.info("In method retrieveAllStocks");
+		log.info("In method retrieveAllStocks");
 		List<Stock> stocks = (List<Stock>) stockRepository.findAll();
 		for (Stock stock : stocks) {
 			log.info(" Stock : " + stock);
 		}
-		//log.info("out of method retrieveAllStocks");
+		log.info("out of method retrieveAllStocks");
 		// récuperer la date à l'instant t2
 		// temps execution = t2 - t1
 		return stocks;
@@ -34,54 +34,53 @@ public class StockServiceImpl implements IStockService {
 	@Override
 	public Stock addStock(Stock s) {
 		// récuperer la date à l'instant t1
-		//log.info("In method addStock");
-		stockRepository.save(s);
-		return s;
+		log.info("In method addStock");
+		return stockRepository.save(s);
 		
 	}
 
 	@Override
 	public void deleteStock(Long stockId) {
-		//log.info("In method deleteStock");
+		log.info("In method deleteStock");
 		stockRepository.deleteById(stockId);
 
 	}
 
 	@Override
 	public Stock updateStock(Stock s) {
-		//log.info("In method updateStock");
+		log.info("In method updateStock");
 		return stockRepository.save(s);
 	}
 
 	@Override
 	public Stock retrieveStock(Long stockId) {
 		long start = System.currentTimeMillis();
-		//log.info("In method retrieveStock");
+		log.info("In method retrieveStock");
 		Stock stock = stockRepository.findById(stockId).orElse(null);
-		//log.info("out of method retrieveStock");
+		log.info("out of method retrieveStock");
 		 long elapsedTime = System.currentTimeMillis() - start;
-		//log.info("Method execution time: " + elapsedTime + " milliseconds.");
+		log.info("Method execution time: " + elapsedTime + " milliseconds.");
 
 		return stock;
 	}
 
 	@Override
 	public String retrieveStatusStock() {
-		//SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-		//Date now = new Date();
-		//String msgDate = sdf.format(now);
-		//String finalMessage = "";
-		//String newLine = System.getProperty("line.separator");
-		//List<Stock> stocksEnRouge = (List<Stock>) stockRepository.retrieveStatusStock();
-		//for (int i = 0; i < stocksEnRouge.size(); i++) {
-			//finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
-				//	+ stocksEnRouge.get(i).getLibelleStock() + " a une quantité de " + stocksEnRouge.get(i).getQte()
-					//+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQteMin()
-					//+ newLine;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		Date now = new Date();
+		String msgDate = sdf.format(now);
+		String finalMessage = "";
+		String newLine = System.getProperty("line.separator");
+		List<Stock> stocksEnRouge = (List<Stock>) stockRepository.retrieveStatusStock();
+		for (int i = 0; i < stocksEnRouge.size(); i++) {
+			finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
+					+ stocksEnRouge.get(i).getLibelleStock() + " a une quantité de " + stocksEnRouge.get(i).getQte()
+					+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQteMin()
+					+ newLine;
 
-		//}
-		//log.info(finalMessage);
-		return "finalMessage";
+		}
+		log.info(finalMessage);
+		return finalMessage;
 	}
 
 }
