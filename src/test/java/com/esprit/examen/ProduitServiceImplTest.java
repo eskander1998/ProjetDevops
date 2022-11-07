8b9d6eb0b5e6af022ae17326fa6e295bd26af77f
karoui ahmed <ahmed.karoui@esprit.tn>
2022-11-07 07:22:10 +0100

    update projet 34

--- src/test/java/com/esprit/examen/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/ProduitServiceImplTest.java
@@ -11,7 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.runner.RunWith;
@@ -36,68 +39,19 @@ import com.esprit.examen.services.StockServiceImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes=ProduitServiceImplTest.class)
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 
 public class ProduitServiceImplTest {
     @Autowired
        IProduitService produitService;
   
        @Test
+       @Order(1)
        public void testRetrieveAllProduit() throws ParseException {
 
          List<Produit> produit = produitService.retrieveAllProduits();
-         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-         Date date11 = dateFormat.parse("30/09/2000");
-         Date date12 = dateFormat.parse("30/09/2010");
-         Produit sa = new Produit(date11, date12);
-         Produit savedProduit = produitService.addProduit(sa);
-         log.info(" Produit : " + sa);
-
-         assertNotNull(sa.getIdProduit());
-
-         produitService.retrieveProduit(sa.getIdProduit());
-
-         List<Produit> Produits = produitService.retrieveAllProduits();
-         log.info("Les  Produits : " + Produits);
+         Assertions.assertEquals(0,  produit.size());
       }
-   @Test
-   public void testDeleteProduit() throws ParseException {
-/*
-      List<Produit> Produits = produitService.retrieveAllProduits();
-         int expected = Produits.size();
-         Long idFour=(long) 2;
-         //assertEquals(expected + 1, produitService.retrieveAllProduits().size());
-      System.out.print("size "+Produits.size());
-         log.info(" count" + Produits.size());
-      produitService.deleteProduit(1L);
-      log.info("size2 "+produitService.retrieveAllProduits().size());
-*/
-
-   }
-   @Test
-   public void testAddProduit() throws ParseException{
-      List<Produit> Produits = produitService.retrieveAllProduits();
-      SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-      Date date1 = dateFormat.parse("10/02/2020");
-      Date date2 = dateFormat.parse("12/09/2022");
-      Produit p = new Produit(date1, date2);
-      log.info("produit \n "+p);
-
-      Produit savedProduit= produitService.addProduit(p);
-      log.info("size1 "+Produits.size());
-      System.out.print("size1 "+Produits.size());
-      produitService.deleteProduit(savedProduit.getIdProduit());
-      List<Produit> Produits1 = produitService.retrieveAllProduits();
-
-      log.info("size2 "+Produits1.size());
-   }
-   //@Test
-	//void addProduit(){
-		//Produit produit = new Produit("produit test",10);
-	    //Mockito.doReturn(produit).when(produitRepository).save(produit);
-	    //Produit produit1 = PSI.addProduit(produit);
-	    //Assertions.assertNotNull(produit1);
-	  //  }
 }
